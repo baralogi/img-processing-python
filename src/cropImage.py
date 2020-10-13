2e2cb5e286102204dc31fa32b0b709dc16cbeecc
Sebastianus Sembara <sembara9090@gmail.com>
2020-10-13 15:21:33 +0700

    initial commit

---
+++ src/cropImage.py
@@ -0,0 +1,26 @@
+from PIL import Image
+
+# mengambil gambar
+origin = Image.open("./peppers.tif")
+
+# menampilkan gambar
+origin.show() 
+
+# get orginal dimensions
+width, height = origin.size 
+
+imgCr11 = origin.crop((0,0,width/2,height/2));
+imgCr12 = origin.crop((width/2,0,width,height/2));
+imgCr21 = origin.crop((0,height/2,width/2,height));
+imgCr22 = origin.crop((width/2,height/2,width,height));
+
+# save image
+imgCr11.save('./imgCr11.jpg');
+imgCr12.save('./imgCr12.jpg');
+imgCr21.save('./imgCr21.jpg');
+imgCr22.save('./imgCr22.jpg');
+
+imgCr11.show()
+imgCr12.show()
+imgCr21.show()
+imgCr22.show()
---
+++ src/showImage.py
@@ -0,0 +1,7 @@
+from PIL import Image
+
+#mengambil gambar
+citra = Image.open("./waterfall.bmp")
+
+#menampilkan gambar
+citra.show() 
