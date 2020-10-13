c05a6ff39dec70783022e670853908cb5bc1895e
Sebastianus Sembara <sembara9090@gmail.com>
2020-10-13 15:31:35 +0700

    add comment to understand

--- src/cropImage.py
+++ src/cropImage.py
@@ -9,6 +9,7 @@ origin.show()
 # get orginal dimensions
 width, height = origin.size 
 
+# crop image by coordinate (x,y) = (left, upper), (x,y) = (right, lower)
 imgCr11 = origin.crop((0,0,width/2,height/2));
 imgCr12 = origin.crop((width/2,0,width,height/2));
 imgCr21 = origin.crop((0,height/2,width/2,height));
@@ -20,6 +21,7 @@ imgCr12.save('./imgCr12.jpg');
 imgCr21.save('./imgCr21.jpg');
 imgCr22.save('./imgCr22.jpg');
 
+# show croppedImage
 imgCr11.show()
 imgCr12.show()
 imgCr21.show()
