477c3ec8aff5f9a30f76ebf90bde698c009ea0df
Sebastianus Sembara <sembara9090@gmail.com>
2020-10-19 00:57:48 +0700

    fix margin

--- src/combineImage.py
+++ src/combineImage.py
@@ -11,15 +11,12 @@ mBottom = 5
 mLeft = 5
 mRight = 5
 mode = 'RGB'
-size = ((width + mLeft + mRight + 5) , (height + mTop + mBottom + 5))
+size = ((width + mLeft + mRight) , (height + mTop + mBottom))
 # size = (600,600)
 color = (255, 255, 0)
 
 # pick background image
 background = Image.new(mode, size, color)
-
-# show background image
-background.show()
 
 # copy background image
 backgroundCopy = background.copy()
@@ -36,8 +33,8 @@ imgCr22 = Image.open("./imgCr22.jpg")
 
 # combine image to backgroundImage
 backgroundCopy.paste(imgCr11,(mLeft, mTop))
-backgroundCopy.paste(imgCr12,(int((width + mLeft + mRight)/2)+5,mRight))
-backgroundCopy.paste(imgCr21,(mLeft,int((height + mTop + mBottom)/2)+5))
-backgroundCopy.paste(imgCr22,(int((width + mLeft + mRight)/2)+5,int((height + mTop + mBottom)/2)+5))
+backgroundCopy.paste(imgCr12,(int((width + mLeft + mRight)/2),mRight))
+backgroundCopy.paste(imgCr21,(mLeft,int((height + mTop + mBottom)/2)))
+backgroundCopy.paste(imgCr22,(int((width + mLeft + mRight)/2),int((height + mTop + mBottom)/2)))
 backgroundCopy.show()
 
